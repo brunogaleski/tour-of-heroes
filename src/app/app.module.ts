13df160ac898fc96b5413ae5e34d55744bc6bd53
Bruno Galeski <bruno.galeski@wipro.com>
2018-05-24 11:05:15 -0300

    Add translation
    Signed-off-by: Bruno Galeski <bruno.galeski@wipro.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
-import { HttpClientModule } from '@angular/common/http' 
+import { HttpClientModule, HttpClient } from '@angular/common/http' 
 import { AppRoutingModule } from './/app-routing.module';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { InMemoryDataService }  from './in-memory-data.service';
 
@@ -13,6 +15,11 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -32,8 +39,18 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+      InMemoryDataService, { 
+        dataEncapsulation: false,
+        passThruUnknownUrl: true 
+      }
+    ),
+    TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,7 +10,7 @@ import { HeroService } from '../services/hero.service';
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
  
-  constructor(private heroService: HeroService) { }
+  constructor (private heroService: HeroService) { }
  
   ngOnInit() {
     this.getHeroes();
